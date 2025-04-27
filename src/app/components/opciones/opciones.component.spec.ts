c4bd4dede0ebe2fb7842b5b98ff77af7d93532f4
Axel Brant Marin <axel.brantmarin@gmail.com>
2025-04-26 21:33:44 -0400

    actualizacion

--- src/app/components/opciones/opciones.component.spec.ts
+++ src/app/components/opciones/opciones.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
 import { OpcionesComponent } from './opciones.component';
 
 describe('OpcionesComponent', () => {
